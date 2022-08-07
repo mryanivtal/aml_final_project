bbfb34079820cb6d7604b57fdd0e5cec9a979d45
mr.yaniv.tal@gmail.com <mr.yaniv.tal@gmail.com>
2022-08-08 00:06:50 +0300

    Part2 begun

---
+++ part2/gp_vae_for_data_generation/utils.py
@@ -0,0 +1,20 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def display_mnist_chars(images_tensor):
+    matplotlib.use('Qt5Agg')
+
+    num_rows = int(images_tensor.shape[1])       # time dimension - same digit rotated
+    num_columns = int(images_tensor.shape[0])    # different original samples
+
+    fig, ax = plt.subplots(num_columns, num_rows)
+    for column in range(num_columns):
+        for image in range(num_rows):
+            pixels = np.array(images_tensor[column, image, :]).reshape((28, 28))
+            ax[column, image].imshow(pixels, cmap='gray')
+
+    plt.show()
+
+
+
